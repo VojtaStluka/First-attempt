a2dd91d0af4284677fb612f31f69d8f63c777bde
VojtaStluka <stlukav@arcig.cz>
2025-02-03 15:21:25 +0100

    3.2.

--- stromy2.py
+++ stromy2.py
@@ -1,43 +1,49 @@
 from __future__ import annotations #řeší definování v definici (Uzel in Uzel)
 from dataclasses import dataclass
+from abc import abstractmethod
 from typing import Optional
+from enum import Enum
 
 class Uzel:
     operace: str
     levy: Uzel | List
     pravy: Uzel| List
-    def tisk(self):
+
+    @abstractmethod
+    def eval(self) -> float: #kontrakt
         ...
+
+class Operace(Enum):
+    PLUS = 0
+    MINUS = 1
+    KRAT = 2
+    DELENO = 3
+
 
 @dataclass
 class UOperace(Uzel):
-    operace: str
+    operace: Operace
     levy: Uzel | List
     pravy: Uzel| List
-    def tisk(self):
-        print("Jsem vnitřní uzel")
-        self.levy.tisk()
-        print(self.operace)
-        self.pravy.tisk()
+
+    def eval(self) -> float:
+        if self.operace == Operace.PLUS:
+            return self.levy.eval() + self.pravy.eval()
+        elif self.operace == Operace.MINUS:
+            return self.levy.eval() - self.pravy.eval()
+        elif self.operace == Operace.KRAT:
+            return self.levy.eval() * self.pravy.eval()
+        else:
+            return self.levy.eval() / self.pravy.eval()
 
 @dataclass
 class List(Uzel):
     hodnota: int
-    def tisk(self):
-        print(self.hodnota)
 
-
-'''#operace (3+2)*4 - 8
-tri = Uzel('3')
-dva = Uzel ('2')
-levy2= Uzel ('+',tri,dva)
-ctyri = Uzel ('4')
-levy=Uzel('*',levy2,ctyri)
-osm = Uzel ('8')
-minus=Uzel('-',levy,osm)'''
-
+    def eval(self) -> float:
+        return self.hodnota
 
 uzel10=List(10)
 uzel8=List(8)
-uzelplus=UOperace('+',uzel10,uzel8)
-uzelplus.tisk()
+uzelplus=UOperace(Operace.KRAT,uzel10,uzel8)
+print(uzelplus.eval())
