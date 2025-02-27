ece88ad5c59164f24e9a0f8a66dce38a80d13858
VojtaStluka <stlukav@arcig.cz>
2025-02-27 14:29:09 +0100

    Skoro...

--- Facebook.py
+++ Facebook.py
@@ -19,8 +19,27 @@ class Facebook:
         user1_uzel.znamost.append(user2_uzel)
         user2_uzel.znamost.append(user1_uzel)
         
-    def jak_daleko(name1, name2) -> None:
-        ...
+    def jak_daleko(self,jmeno1,jmeno2) -> None:
+        if jmeno1 == jmeno2:
+            return 0  # Pokud jsou to stejní lidé, vzdálenost je 0
+        
+        # Fronta pro BFS, každá položka bude obsahovat (uživatel, vzdálenost)
+        queue = deque([(self._users[jmeno1], 0)])  #list jména a vzdálenosti
+        # Množina pro návštěvu (abychom se nevraceli zpět na již navštívené uživatele)
+        visited = set([name1]) #funkce set() vždy má pouze jednou konkrétní hodnotu
+        
+        while queue:
+            current_user, distance = queue.popleft()    # z listu bude jménu pod proměnnou current_user a vzdálenost v distance
+            
+            # Procházení všech známých uživatelů
+            for neighbor in current_user.znamost:
+                if neighbor.jmeno == jmeno2:
+                    return distance + 1      # Našli jsme jméno name2, vrátíme vzdálenost, tzn. přímo přítel; fronta je tímto prázdná
+                if neighbor.jmeno not in visited: # Jestliže není v seznamu již prošlých lidí
+                    visited.add(neighbor.jmeno) #Přidáme
+                    queue.append((neighbor, distance + 1)) #Vrátíme zpět do fronty, čili cyklus se opakuje a máme vzdálenost >1
+        
+        return None  # Pokud jsme nenašli propojení
 
 # Vytvoření instance Facebooku
 fb = Facebook()
@@ -49,3 +68,7 @@ znamosti = [("Adam", "Beata"), ("Adam", "Cyril"), ("Beata", "Dana"),
 # Vkládání známostí do Facebooku
 for clovek1, clovek2 in znamosti:
     fb.pridej_znamost(clovek1, clovek2)
+
+#Zkouška funkčnosti
+vzdalenost = fb.jak_daleko("Adam", "Dana")
+print(vzdalenost) 
