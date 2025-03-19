63a0d871fce317364d968878eae7302e0391b58e
VojtaStluka <stlukav@arcig.cz>
2025-03-19 22:04:52 +0100

    Not done, 19.3.

--- Dijkstrův.py
+++ Dijkstrův.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from typing import Any
 class Mesto:
     def __init__(self,nazev:str):
         self.nazev=nazev
@@ -27,35 +29,54 @@ class Graph:
         for mesto in self._mesta:
             fronta.append([mesto,self.soused])
             visited = set([mesto])
-
+            
 @dataclass
 class Element:
-    value:object
-    priority:int
+    value: Any
+    priority: int
+ 
+class BinaryHeap:
 
-class PriorityQueue:
     def __init__(self):
-        self.seznam=[]
+        self.heap: list[Element] = []
+        
+    def push(self,element:Element):
+        self.heap.append(element)
+        idx=(len(self.heap))-1
+        while idx>0:
+            idx_rodic = (idx-1) // 2
+            rodic = self.heap[idx_rodic] 
+            dite = element           
+            if rodic.priority > dite.priority:
+                self.heap[idx] = rodic
+                self.heap[idx_rodic] = dite
+            else: 
+                break
+         
 
-   def push (self, prvek):
-       self.seznam.append(prvek)
-       idx=len(self.seznam)
-       dite=prvek
-       if idx>1:
-           while True:
-               idx_rodic = (idx -1) // 2
-               rodic = self.seznam[idx_rodic]
-               if rodic.priority > dite.priority:
-                   self.seznam[idx_dite] = rodic
-                   self.seznam[idx_rodic] = dite
-                   idx_dite = idx_rodic
-               else:
-                   break      
-    def pop():
-        ...
+    def pop(self):
+        min_element=self.heap[0]
+        last_element=self.heap.pop()
+        if not self.heap: 
+            return min_element
+        else:
+            self.heap[0]=last_element
+            idx=0
 
-a=PriorityQueue()
-a.push(4)
+            while True:
+                smallest=idx
+                left_child = 2 * idx + 1
+                right_child = 2 * idx + 2
+                if left_child <= len(self.heap)-1 and self.heap[left_child].priority < self.heap[smallest].priority:
+                        smallest = left_child
+                if  right_child <= len(self.heap)-1 and self.heap[right_child].priority < self.heap[smallest].priority:
+                        smallest = right_child
+                if smallest != idx:
+                    self.heap[idx], self.heap[smallest] = self.heap[smallest], self.heap[idx]
+                    idx = smallest
+                else: 
+                    break
+            return min_element
         
 def init_distances() -> Graph:
         graph = Graph()
