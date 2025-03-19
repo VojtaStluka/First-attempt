0b587643c7cdae4219758e9a903827f54280acb5
VojtaStluka <stlukav@arcig.cz>
2025-03-19 22:16:05 +0100

    19.3.

---
+++ Pokusy.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class Element:
+    value: Any
+    priority: int
+ 
+class BinaryHeap:
+
+    def __init__(self):
+        self.heap: list[Element] = []
+        
+    def push(self,element:Element):
+        self.heap.append(element)
+        idx=(len(self.heap))-1
+        while idx>0:
+            idx_rodic = (idx-1) // 2
+            rodic = self.heap[idx_rodic] 
+            dite = element           
+            if rodic.priority > dite.priority:
+                self.heap[idx] = rodic
+                self.heap[idx_rodic] = dite
+            else: 
+                break
+         
+
+    def pop(self):
+        min_element=self.heap[0]
+        last_element=self.heap.pop()    #najdeme poslední prvek
+        if not self.heap: #Pokud jsme odstranili poslední prvek, vrátíme rovnou
+            return min_element
+        else:
+            self.heap[0]=last_element   #prohodíme do kořene
+            idx=0
+
+            while True:
+                smallest=idx
+                left_child = 2 * idx + 1
+                right_child = 2 * idx + 2
+                if left_child <= len(self.heap)-1 and self.heap[left_child].priority < self.heap[smallest].priority:    #kontrola min_heap a zároveň kontrola, jestli existuje (v poslední vrstvě být nutně nemusí)
+                        smallest = left_child
+                if  right_child <= len(self.heap)-1 and self.heap[right_child].priority < self.heap[smallest].priority:
+                        smallest = right_child
+                if smallest != idx:
+                    self.heap[idx], self.heap[smallest] = self.heap[smallest], self.heap[idx]   #prohození ve vrstvách
+                    idx = smallest
+                else: 
+                    break
+            return min_element
+
+
+    def head(self):
+        if not self.heap:
+            raise Exception("Heap is empty")
+        min_idx = 0
+        print(self.heap[min_idx])
+       
+'''
+bh = BinaryHeap()
+bh.push(Element("task1", 3))
+bh.push(Element("task2", 1))
+bh.push(Element("task3", 2))
+
+print(bh.pop())  # Mělo by vrátit Element("task2", 1)
+print(bh.pop())  # Mělo by vrátit Element("task3", 2)
+print(bh.pop())  # Mělo by vrátit Element("task1", 3)'''
+
+def push(heap, priority):
+    heap.push(Element(priority, priority))
+
+def test_push() -> None:
+    heap = BinaryHeap()
+    push(heap, 5)
+    assert heap.head().priority == 5
+    push(heap, 8)
+    assert heap.head().priority == 5
+    push(heap, 4)
+    assert heap.head().priority == 4
+    push(heap, 7)
+    assert heap.head().priority == 4
+    push(heap, 10)
+    assert heap.head().priority == 4
+    push(heap, 2)
+    assert heap.head().priority == 2
+
+def test_push_pop() -> None:
+    heap = BinaryHeap()
+    push(heap, 5)
+    push(heap, 8)
+    push(heap, 4)
+    assert heap.pop().priority == 4
+    assert heap.head().priority == 5
+
+    # another push round
+    push(heap, 7)
+    push(heap, 10)
+    push(heap, 2)
+    assert heap.pop().priority == 2
+    assert heap.pop().priority == 5
+    assert heap.pop().priority == 7
+    assert heap.pop().priority == 8
+    assert heap.pop().priority == 10
